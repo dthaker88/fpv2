d7902391ae25fd08038ad0767817deb0c5fcafd7
Dipesh Thaker <dt36@afsconnect2.njit.edu>
2017-12-16 18:47:08 -0500

    updated

--- mvc-mvc2/pages/todo.php
+++ mvc-mvc2/pages/todo.php
@@ -28,9 +28,9 @@
 
 
     Owner Email: <input type="text" name="owneremail" value = "<?php echo $data -> owneremail; ?>"><br>
-    Due Date: <input type="text" name="duedate" value = "<php $data->duedate; ?>"><br>
-    Message: <input type="text" name="message" value = "<php $data->message; ?>"><br>
-    Is done?: <input type="text" name="isdone" value = "<php $data->isdone; ?>"><br>
+    Due Date: <input type="text" name="duedate" value = "<?php echo $data->duedate; ?>"><br>
+    Message: <input type="text" name="message" value = "<?php echo  $data->message; ?>"><br>
+    Is done?: <input type="text" name="isdone" value = "<?php echo $data->isdone; ?>"><br>
     <input type="submit" value="Submit form">
 </form>
 
