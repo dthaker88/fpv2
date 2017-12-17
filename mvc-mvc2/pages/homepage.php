1e6759b74830d9745e17f1b038910fa6dddab92b
Dipesh Thaker <dt36@afsconnect2.njit.edu>
2017-12-17 12:34:19 -0500

    updated

--- mvc-mvc2/pages/homepage.php
+++ mvc-mvc2/pages/homepage.php
@@ -28,13 +28,14 @@
 
     ?> </h1>
 
-<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
+
 
 <?php
 session_start();
 if(key_exists('userID', $_SESSION)) {
     echo '<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>';
     echo '<h1><a href="index.php?page=accounts&action=logout">Log Out</a></h1>';
+    echo '<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>';
     //echo '<h1><a href="index.php?page=tasks&action=logout">Log Out</a></h1>';
 }
     ?>
